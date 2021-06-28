a34cc0df5bc4664764059386db3b64ce18f765fb
Adam Vaughn <adammathewvaughn>
2021-06-28 12:20:31 -0500

    final cut

--- script.js
+++ script.js
@@ -1,36 +1,59 @@
-//let Promise = new Promise((resolve, reject) => {});
+//? const math=slowMath.add(6,2)
+//?    .then((a)=>{ 
+//?        console.log(a);
+//?        return slowMath.multiply(a,2); 
+//?    }).then((b)=>{
+//?        console.log(b);
+//?        return slowMath.divide(b,4);
+//?    }).then((c)=>{
+//?        console.log(c);
+//?        return slowMath.subtract(c,3);        
+//?    }).then((d)=>{
+//?        console.log(d);
+//?        return slowMath.add(d,98)
+//?    }).then((e)=>{
+//?        console.log(e);
+//?        return slowMath.remainder(e,2);
+//?    }).then((f)=>{
+//?        console.log(f);
+//?        return slowMath.multiply(f,50);
+//?    }).then((g)=>{
+//?        console.log(g);
+//?        return slowMath.remainder(g,40);
+//?    }).then((h)=>{
+//?        console.log(h);
+//?        return slowMath.add(h,32)
+//?    }).then((i)=>{
+//?        console.log(`The final result is ${i}`)
+//?    }).catch((err)=>{
+//?        console.error(err)
+//?    })    
 
 
-
-let val=(slowMath.add(6,2));
-return (val);
-
-    //slowMath.multiply(val1, 2); 
-    // slowMath.divide(val1, 4); 
-// slowMath.subtract(val1, 3); 
-// slowMath.remainder(val1, val2); 
-// slowMath.add(val, 98);
-// slowMath.remainder(val, 2);
-// slowMath.multiply(val, 50);
-// slowMath.remainder(val, 40);
-// slowMath.add(val, 32);
-//? console.log(`The final result is ${endresult}`)
-
-//In your JavaScript file, create a Promise chain that uses SlowMath to perform all of the following operations, in this exact order:
-//// 1. Add together 6 and 2.
-// TODO:2. Wait on the result, log it, and then multiply the result by 2.
-// 3. Wait on the result, log it, and then divide the result by 4.
-//? 4. Wait on the result, log it, and then subtract 3 from the result.
-// 5. Wait on the result, log it, and then add 98 to the result.
-// 6. Wait on the result, log it, and then find the remainder from dividing the result by 2.
-// 7. Wait on the result, log it, and then multiply the result by 50.
-// 8. Wait on the result, log it, and then find the remainder from dividing the result by 40.
-// 9. Wait on the result, log it, and then add 32 to the result.
-// 10. Wait on the result, and then log: The final result is \_\_\_, where you fill in the blank with the final result.
-// 11. Add a `.catch` function to handle any errors that occur. Log the error to the console.
-
-
-//TODO Comment out the previous code you wrote.
-//TODO Write an asynchronous function named doMath() that you will invoke to execute the slowMath operations to get 42 via the same steps as above.
-//TODO Remember to wrap your await promise chains in a try/catch block.
-//TODO Only use async/await, no .then() is needed in this part!
+    let doMath =async()=>{
+        try{
+        let math = await slowMath.add(6,2);
+            console.log(math);
+        math= await slowMath.multiply(math,2);
+            console.log(math);
+        math= await slowMath.divide(math,4)
+            console.log(math);
+        math= await slowMath.subtract(math,3)
+            console.log(math);
+        math= await slowMath.add(math,98)
+            console.log(math);
+        math= await slowMath.remainder(math,2)
+            console.log(math);
+        math= await slowMath.multiply(math,50)
+            console.log(math);
+        math= await slowMath.remainder(math,40)
+            console.log(math);
+        math= await slowMath.add(math,32)
+            console.log(`The final result is ${math}`) 
+        }
+        catch(err){
+            console.error(err)
+        } 
+    }
+    doMath();
+    
